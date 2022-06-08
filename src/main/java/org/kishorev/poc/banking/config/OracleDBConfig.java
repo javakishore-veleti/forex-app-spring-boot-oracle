d7e116b007c9215d327d9c2f79e5d794dbdba738
a_k_k <arunakishore@outlook.com>
2022-06-08 05:09:29 -0400

    Initial Commit

--- src/main/java/org/kishorev/poc/banking/config/OracleDBConfig.java
+++ src/main/java/org/kishorev/poc/banking/config/OracleDBConfig.java
@@ -1,8 +1,10 @@
 package org.kishorev.poc.banking.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class OracleDBConfig {
 
 }
--- src/main/java/org/kishorev/poc/banking/entity/TradingBookInfo.java
+++ src/main/java/org/kishorev/poc/banking/entity/TradingBookInfo.java
@@ -1,4 +1,22 @@
 package org.kishorev.poc.banking.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TRADING_BOOK")
+@Getter
+@Setter
 public class TradingBookInfo {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TRADING_BOOK_SEQ")
+    @SequenceGenerator(sequenceName = "trading_book_seq", allocationSize = 1, name = "TRADING_BOOK_SEQ")
+    Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
 }
